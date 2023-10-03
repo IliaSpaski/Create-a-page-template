cd373a8987b7c87862ff20223f2df2611cd05bb4
Ilya <iloa.spaski123321@gmail.com>
2023-10-03 21:08:44 +0300

    Errors have been corrected and a server script
    Errors have been corrected and a server script has been added so that when it is launched, an array of objects with a random number of students and their data in the amount of 7 positions is generated

--- server.js
+++ server.js
@@ -1,32 +1,47 @@
+//Перед прочтением или изменением кода ознакомьтесь с лицензированием кода!!!!!!!!!!
+//CC Attribution — Noncommercial — No Derivative Works (сокращённо CC BY-NC-ND) АКТ CC BY-NC-ND 4.0
+//https://creativecommons.org/licenses/by-nc-nd/4.0/
+//Creator - Spasky Ilya
+
+// Подключаем необходимые модули Node.js
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
 
-// MIME-типы
+// MIME-типы файлов, используемые для отправки в HTTP-ответе
 const contentTypes = {
     ".ico": "image/x-icon",
     ".jpg": "image/jpeg",
     ".jpeg": "image/jpeg",
     ".png": "image/png",
     ".gif": "image/gif",
-
     ".html": "text/html",
     ".css": "text/css",
     ".js": "text/javascript",
 };
+
+// Массивы для имен, фамилий и отчеств
+const names = ['Иван', 'Анна', 'Петр', 'Мария', 'Алексей'];
+const surnames = ['Иванов', 'Петров', 'Сидоров', 'Смирнов', 'Козлов'];
+const patronymics = ['Иванович', 'Петрович', 'Алексеевич', 'Дмитриевич', 'Сергеевич'];
+const imagePaths = [
+    'img/People2.jpg',
+    'img/People3.jpg',
+    'img/People1.jpg',
+    'img/avatar.png'
+];
 
 // Функция для генерации случайного целого числа в заданном диапазоне
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// Массивы для имен, фамилий и отчеств
-const names = ['Иван', 'Анна', 'Петр', 'Мария', 'Алексей'];
-const surnames = ['Иванов', 'Петров', 'Сидоров', 'Смирнов', 'Козлов'];
-const patronymics = ['Иванович', 'Петрович', 'Алексеевич', 'Дмитриевич', 'Сергеевич'];
-
-// Функция для генерации случайных данных
+// Функция для генерации случайных данных о студенте
 function generateRandomData() {
+    // Генерируем случайный путь к изображению
+    const randomIndex = getRandomInt(0, imagePaths.length - 1);
+    const randomImagePath = imagePaths[randomIndex];
+
     // Генерируем случайное ФИО
     const randomName = names[Math.floor(Math.random() * names.length)];
     const randomSurname = surnames[Math.floor(Math.random() * surnames.length)];
@@ -58,13 +73,29 @@ function generateRandomData() {
         achievement3: achievementText3,
         achievement4: achievementText4,
         group: group,
+        rex: randomImagePath,
     };
 }
+
+function generateRandomStudents(count) {
+    const students = [];
+
+    for (let i = 0; i < count; i++) {
+        const studentData = generateRandomData(); // Создаем новый объект данных на каждой итерации
+        students.push(studentData);
+    }
+
+    return students;
+}
+
+
+// Создаем HTTP-сервер
 const server = http.createServer(function (request, response) {
     console.log(`Url: ${request.url}`);
 
     switch (request.url) {
         case "/":
+            // Если URL - "/", выполняется перенаправление на "/index.html"
             response.writeHead(301, {
                 Location: "/index.html",
                 "Content-Type": "text/html; charset=utf-8",
@@ -74,20 +105,82 @@ const server = http.createServer(function (request, response) {
 
 
         case "/rand_student":
+            // Если URL - "/rand_student", генерируются случайные данные о студенте и отправляется HTML-страница с ними
             response.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
 
-            // Генерируем случайные данные о студенте
-            const randomData = generateRandomData();
+            // Генерируем случайное количество студентов (например, 7)
+            const numberOfStudents = 7;
+            const randomStudents = generateRandomStudents(numberOfStudents);
 
-            // Генерируем случайный путь к изображению
-            const imagePaths = [
-                'img/People2.jpg',
-                'img/People3.jpg',
-                'img/People1.jpg',
-                'img/avatar.png'
-            ];
-            const randomIndex = getRandomInt(0, imagePaths.length - 1);
-            const randomImagePath = imagePaths[randomIndex];
+            let studentsTable = "";
+            randomStudents.forEach((student, index) => {
+                studentsTable += `
+                    <div class="container">
+                        <h1>Информация о пользователе MyStat</h1>
+                        <img id="avatar1" class="img" src="${student.rex}" alt="Аватар пользователя">
+                        <table>
+                            <tr>
+                                <th>Параметр</th>
+                                <th>Значение</th>
+                            </tr>
+                            <tr>
+                                <td>ФИО:</td>
+                                <td id="userName">${student.name}</td>
+                            </tr>
+                            <tr>
+                                <td>Группа:</td>
+                                <td id="userGroup">${student.group}</td>
+                            </tr>
+                            <tr>
+                                <td class="no">
+                                    <div>
+                                        <p>Достижения:</p>
+                                    </div>
+                                </td>
+                                <td class="div">
+                                    <div class="achievement">
+                                        <img src="img/diamond.png" alt="Достижение 1">
+                                        <p id="achievement1">${student.achievement1}</p>
+                                    </div>
+                                    <div class="achievement">
+                                        <img src="img/fre.png" alt="Достижение 2">
+                                        <p id="achievement2">${student.achievement2}</p>
+                                    </div>
+                                    <div class="achievement">
+                                        <img src="img/Comment.png" alt="Достижение 3">
+                                        <p id="achievement3">${student.achievement3}</p>
+                                    </div>
+                                    <div class="achievement">
+                                        <img src="img/medal.png" alt="Достижение 4">
+                                        <p id="achievement4">${student.achievement4}</p>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Возраст:</td>
+                                <td id="userAge">${student.age}</td>
+                            </tr>
+                            <tr>
+                                <td>Email</td>
+                                <td id="userEmail">${student.email}</td>
+                            </tr>
+                            <tr>
+                                <td>Телефон</td>
+                                <td id="userPhone">${student.phone}</td>
+                            </tr>
+                            <tr>
+                                <td>Идентификатор</td>
+                                <td id="userId">${student.id}</td>
+                            </tr>
+                            <tr>
+                                <td>Был последний раз:</td>
+                                <td id="lastVisited">${student.visited}</td>
+                            </tr>
+                        </table>
+                    </div>
+   
+            `;
+            });
 
             // Отправляем HTML-страницу с данными
             response.write(`
@@ -101,69 +194,12 @@ const server = http.createServer(function (request, response) {
                         <script src="js/script.js" defer></script>
                     </head>
                     <body>
-                        <div class="container">
-                            <h1>Информация о пользователе MyStat</h1>
-                            <img id="avatar1" class="img" src="${randomImagePath}" alt="Аватар пользователя">
-                            <table>
-                                <tr>
-                                    <th>Параметр</th>
-                                    <th>Значение</th>
-                                </tr>
-                                <tr>
-                                    <td>ФИО:</td>
-                                    <td id="userName">${randomData.name}</td>
-                                </tr>
-                                <tr>
-                                    <td>Группа:</td>
-                                    <td id="userGroup">${randomData.group}</td>
-                                </tr>
-                                <tr>
-                                    <td class="no">
-                                        <div>
-                                            <p>Достижения:</p>
-                                        </div>
-                                    </td>
-                                    <td class="div">
-                                        <div class="achievement">
-                                            <img src="img/diamond.png" alt="Достижение 1">
-                                            <p id="achievement1">${randomData.achievement1}</p>
-                                        </div>
-                                        <div class="achievement">
-                                            <img src="img/fre.png" alt="Достижение 2">
-                                            <p id="achievement2">${randomData.achievement2}</p>
-                                        </div>
-                                        <div class="achievement">
-                                            <img src="img/Comment.png" alt="Достижение 3">
-                                            <p id="achievement3">${randomData.achievement3}</p>
-                                        </div>
-                                        <div class="achievement">
-                                            <img src="img/medal.png" alt="Достижение 4">
-                                            <p id="achievement4">${randomData.achievement4}</p>
-                                        </div>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>Возраст:</td>
-                                    <td id="userAge">${randomData.age}</td>
-                                </tr>
-                                <tr>
-                                    <td>Email</td>
-                                    <td id="userEmail">${randomData.email}</td>
-                                </tr>
-                                <tr>
-                                    <td>Телефон</td>
-                                    <td id="userPhone">${randomData.phone}</td>
-                                </tr>
-                                <tr>
-                                    <td>Идентификатор</td>
-                                    <td id="userId">${randomData.id}</td>
-                                </tr>
-                                <tr>
-                                    <td>Был последний раз:</td>
-                                    <td id="lastVisited">${randomData.visited}</td>
-                                </tr>
-                            </table>
-                        </div>
+                    <div class="container">
+                        <h1>Информация о студентах</h1>
+                    <table>
+                        ${studentsTable}
+                    </table>
+                </div>
                     </body>
                     </html>
                 `);
@@ -171,17 +207,19 @@ const server = http.createServer(function (request, response) {
             break;
 
         default:
+            // Если URL не соответствует вышеуказанным, пытаемся найти и отправить соответствующий файл
             const filePath = path.join("./public", request.url.substring(1));
             console.log(filePath);
 
             fs.access(filePath, fs.constants.R_OK, (err) => {
                 if (err) {
+                    // Если файл не найден, отправляем ошибку 404
                     response.writeHead(404, {
                         "Content-Type": "text/html; charset=utf-8",
                     });
-
                     response.end("<h1>Not found</h1><h2>/rand_student</h2>");
                 } else {
+                    // Если файл найден, определяем его MIME-тип и отправляем его содержимое
                     const extname = path.extname(filePath);
                     const contentType =
                         contentTypes[extname] || "application/octet-stream";
@@ -195,8 +233,8 @@ const server = http.createServer(function (request, response) {
     }
 });
 
+// Указываем порт, на котором будет работать сервер, и запускаем его
 const port = 1313;
 server.listen(port, function () {
     console.log(`Server is running on http://127.0.0.1:${port}`);
 });
-
